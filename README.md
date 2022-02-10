aaeb9b53582658c184d3571e71594a6cec53ef54
gorret <nabatanzigorret143@gmail.com>
2022-02-10 15:29:23 +0300

    Web form validation Assignment Two

---
+++ README.md
@@ -0,0 +1,4 @@
+Survey Form Validation
+Am using form validation using javascript html validation attributes like required for a user to fill that specific filed and maxlength to make username characters not more than 15 and the required mulitple validation field for the select dropdown validation and if all fileds are true the alert "Submitted!" is being displayed
+
+Am using in-line styling for my form
---
+++ main.js
@@ -0,0 +1,27 @@
+function survey_form() {
+
+    // if (document.surveyform.firstname.value == "") {
+    //     alert("Please enter your first name!");
+    //     document.surveyform.firstname.focus();
+    //     return false;
+    // }
+
+    // if (document.surveyform.email.value == "") {
+    //     alert("Please enter your Email!");
+    //     document.surveyform.email.focus();
+    //     return false;
+    // }
+
+    // if (document.surveyform.lastname.value == "") {
+    //     alert("Please enter your lastname!");
+    //     return false;
+    // }
+
+
+
+    // if (document.survey_form.favourites.selectedIndex == 0) {
+    //     alert("Please select your favourite course.");
+    //     return false;
+    // }
+    return alert("Submitted!")
+}
